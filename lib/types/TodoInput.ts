51d37ede69b5b9040e074bedc93c057650ad45f6
Adacchi3 <adachi.prog@gmail.com>
2022-12-17 23:21:30 +0900

    [feat] add react-hook-form

---
+++ lib/types/TodoInput.ts
@@ -0,0 +1,5 @@
+export type TodoInput = {
+  title: string
+  description: string
+  date: Date
+}
--- ui/Form.tsx
+++ ui/Form.tsx
@@ -1,6 +1,13 @@
+'use client'
+import { useForm } from 'react-hook-form'
+import { TodoInput } from '@/lib/types/TodoInput'
+
 const Form = () => {
+  const { register, handleSubmit } = useForm<TodoInput>()
+  const onSubmit = handleSubmit((data) => console.log(data))
+
   return (
-    <form className="rounded bg-white p-4 shadow-xl">
+    <form onSubmit={onSubmit} className="rounded bg-white p-4 shadow-xl">
       <div className="form-control w-full max-w-xs">
         <label className="label">
           <span className="label-text">TODO</span>
@@ -8,6 +15,7 @@ const Form = () => {
         <input
           type="text"
           placeholder="やること"
+          {...register('title')}
           className="form-input input-bordered input w-full max-w-xs"
         />
         <label className="label">
@@ -20,6 +28,7 @@ const Form = () => {
         </label>
         <textarea
           className="form-textare textarea-bordered textarea h-24"
+          {...register('description')}
           placeholder=""
         ></textarea>
       </div>
@@ -29,11 +38,14 @@ const Form = () => {
         </label>
         <input
           type="date"
+          {...register('date')}
           className="form-input input-bordered input w-full max-w-xs"
         />
       </div>
       <div className="form-control mt-4 w-full max-w-xs">
-        <button className="btn-primary btn">登録</button>
+        <button type="submit" className="btn-primary btn">
+          登録
+        </button>
       </div>
     </form>
   )
