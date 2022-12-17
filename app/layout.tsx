f849f0af4e9a50fda26d295824a0d68bcbf6adc1
Adacchi3 <adachi.prog@gmail.com>
2022-12-17 19:52:00 +0900

    [chore] add eslintrc and apply --fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,17 @@
-import '@/styles/dist.css';
-import React from 'react';
+import '@/styles/dist.css'
+import React from 'react'
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html>
       <head>
         <title>Next.js Turbopack App Directory Playground</title>
       </head>
-      <body className="overflow-y-scroll bg-zinc-900">
-        {children}
-      </body>
+      <body className="overflow-y-scroll bg-zinc-900">{children}</body>
     </html>
-  );
+  )
 }
